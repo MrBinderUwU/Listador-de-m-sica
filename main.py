b833cea018dc1995664d36ceaf03ee03103d2189
MrBinderUwU <148299789+MrBinderUwU@users.noreply.github.com>
2024-05-24 10:02:03 -0300

    adicionado suporte para mp3 e melhorias no código
    Ainda não tem o código para que qualquer tipo de extensão de arquivo esteja na pasta sem dar erro. Não achei um jeito de conseguir implementar esta função. porém a maioria dos arquivos mais comuns de um computador não terá problemas quando rodar o programa.

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ import os
 #lista_musica = []
 
 arquivos_salvos = []
+tipos_arquivos = []
 
 def nomear_arquivos(pasta):
     lista_arquivos = os.listdir(pasta)
     for arquivo in lista_arquivos:
-        if ".ogg" in arquivo or ".mp4" in arquivo:
+        if ".ogg" in arquivo or ".mp4" in arquivo or ".mp3" in arquivo:
             if ".ogg" in arquivo:
-                nome_musica = arquivo.split()[-1].replace(".ogg", "")
+                nome_musica = arquivo.replace(".ogg", "")
+            elif ".mp4" in arquivo:
+                nome_musica = arquivo.replace(".mp4", "")
             else:
-                nome_musica = arquivo.split()[-1].replace(".mp4", "")
+                nome_musica = arquivo.replace(".mp3", "")
             nome_musica = nome_musica.replace("_", " ")
             arquivos_salvos.append(nome_musica)
-        elif ".txt" in arquivo or ".toml" in arquivo or ".usm" in arquivo or ".tga" in arquivo or ".bin" in arquivo or ".farc" in arquivo or ".dsc" in arquivo or ".tga" in arquivo or ".dll" in arquivo or ".gif" in arquivo or ".jpg" in arquivo or ".png" in arquivo:
+        elif ".txt" in arquivo or ".toml" in arquivo or ".usm" in arquivo or ".tga" in arquivo or ".bin" in arquivo or ".farc" in arquivo or ".dsc" in arquivo or ".tga" in arquivo or ".dll" in arquivo or ".gif" in arquivo or ".jpg" in arquivo or ".png" in arquivo or ".ini" in arquivo or ".zip" in arquivo or ".exe" in arquivo or ".pdf" in arquivo or ".md" in arquivo or ".docx" in arquivo or ".htm" in arquivo or ".csv" in arquivo or ".jpeg" in arquivo or ".html" in arquivo:
             pass
-        elif ".ogg" not in arquivo or ".mp4" not in arquivo:
+        elif ".ogg" not in arquivo or ".mp4" not in arquivo or "mp3" not in arquivo:
             nomear_arquivos(f'{pasta}/{arquivo}')
 
 #nomear_arquivos("Arquivos")
